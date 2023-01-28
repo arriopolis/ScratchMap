9e731c031e9b7a77b0a85e31c338241921dc6e89
arjan <arjan_rekenmachine@hotmail.com>
2023-01-28 15:50:19 +0100

    Fixed the investigate records file.

--- 03-investigate_records.py
+++ 03-investigate_records.py
@@ -25,7 +25,17 @@ print("Reading", filename, "...")
 with open(filename) as f:
     locations = json.load(f)
 print("Number of locations read:", len(locations))
+
+# Sort the locations chronologically
 locations.sort(key = lambda x : x[2])
+
+# Figure out the ranges of the months
+print("Finding the available months...")
+months = {}
+for i,(_,_,t) in enumerate(locations):
+    idx = tuple(t[:2])
+    months[idx] = i+1
+months_idxs = [(None,0)] + sorted(months.items())
 
 # Parse the country borders
 print("Reading country data...")
@@ -40,51 +50,9 @@ with shapefile.Reader('data/country_borders/ne_10m_admin_0_countries') as shp:
             borders.append(contour)
         countries.append(borders)
 
-# Figure out the ranges of the months
-print("Finding the available months...")
-months = {}
-for i,(_,_,t) in enumerate(locations):
-    idx = tuple(t[:2])
-    months[idx] = i+1
-months_idxs = [(0,0)] + sorted(months.items())
-
-print()("Reading the extra locations in", CONFIG['extra_locations_filename'], "...")
-    day,month,year = 1,1,1970
-    hour,minute,second = 0,0,0
-    ctr = 0
-    num_interpol = 0
-    lasty,lastx = None,None
-    with open(CONFIG['extra_locations_filename']) as f:
-        for line in f:
-            s = line.split('#')[0].strip()
-            if not s: continue
-            op = s.split(' ')[0].strip()
-            vals = ''.join(s.split(' ')[1:])
-            assert op in ['year','month','day','hour','minute','second','add','interpol']
-            if op == 'add':
-                y,x = map(float,vals.split(','))
-                if num_interpol > 0:
-                    for i in range(1,num_interpol+1):
-                        newy = lasty + i*(y-lasty)/(num_interpol+1)
-                        newx = lastx + i*(x-lastx)/(num_interpol+1)
-                        cleaned_locations.append((int(newy*1e7), int(newx*1e7), (year,month,day,hour,minute,second)))
-                        ctr += 1
-                cleaned_locations.append((int(y*1e7), int(x*1e7), (year,month,day,hour,minute,second)))
-                ctr += 1
-                num_interpol = 0
-                lasty,lastx = y,x
-            elif op == 'interpol': num_interpol = int(vals)
-            elif op == 'year': year = int(vals)
-            elif op == 'month': month = MONTHS.index(vals.strip())+1
-            elif op == 'day': day = int(vals)
-            elif op == 'hour': hour = int(vals)
-            elif op == 'minute': minute = int(vals)
-            elif op == 'second': second = int(vals)
-    print
 print("USAGE:")
 print("Click a data point on the map. Its properties are printed in the console.")
 print()
-
 for (_,start),((year,month),end) in zip(months_idxs[:-1],months_idxs[1:]):
     print("Visualize", MONTHS[month-1], "-", year, "?")
     print("Type n to skip this month:", end = ' ')
